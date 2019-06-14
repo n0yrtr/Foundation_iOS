c6f7b555eecc240e06a83ba5c5d22bbe208efcde
Naoya Nishida <n-nishida@zyyx.jp>
2019-06-14 19:26:42 +0900

    first_commit

---
+++ Foundation-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foundation-iOS
+//
+//  Created by naoya nishida on 2019/06/14.
+//  Copyright © 2019 naoya nishida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
