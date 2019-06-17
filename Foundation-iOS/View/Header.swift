6fb5a1ba709d72514cd043f8e170809cd7effeaa
Naoya Nishida <n-nishida@zyyx.jp>
2019-06-17 10:27:46 +0900

    Carthage導入

---
+++ Foundation-iOS/View/Header.swift
@@ -0,0 +1,21 @@
+//
+//  Header.swift
+//  Foundation-iOS
+//
+//  Created by naoya nishida on 2019/06/14.
+//  Copyright © 2019 naoya nishida. All rights reserved.
+//
+
+import UIKit
+
+class Header: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
